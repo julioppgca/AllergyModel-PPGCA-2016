a104cc677e04bc5121c03b9282287d8dfde767cf
Marcio Nicolau <nicolau.3g@gmail.com>
2016-10-20 18:36:11 -0200

    Update parser

--- pre_data.R
+++ pre_data.R
@@ -6,15 +6,28 @@ sapply(libs, require, character.only = TRUE)
 
 pllc_web <- read_html("http://www.atlantaallergy.com/pollen_counts/index/2015/04/01")
 
+# We have interest in these contents of HTML page
+# inside div.calendar-body
 # calendar-day-inner > day-num | count > a
-# 
+
 
 pg <- html_nodes(pllc_web, "div.calendar-body")
 
 pg %>% html_nodes("div.calendar-day-inner") 
 
-vals=pg %>% html_nodes("div.calendar-day-inner")  %>% html_nodes("span.day-num") %>% html_text() 
+days=pg %>% html_nodes("div.calendar-day-inner") %>% 
+  html_nodes("span.day-num") %>% 
+  html_text() %>%
+  gsub(pattern = "\\s+(\\d+)\\s+", replacement = "\\1") %>%
+  as.numeric()
 
-pg %>% html_nodes("div.calendar-day-inner")  %>% html_nodes("a") %>% html_text() %>% as.numeric()
+counts=pg %>% html_nodes("div.calendar-day-inner")  %>% html_nodes("a") %>% html_text() %>% as.numeric()
 
-pg %>% html_nodes("div.calendar-day-inner")  %>% html_nodes("a") %>% html_attr("href") 
+idx = pg %>% html_nodes("div.calendar-day-inner") %>% 
+  html_nodes("a") %>% 
+  html_attr("href") %>% 
+  grep(pattern = "2015/04")
+
+
+dt = data.frame(days=days[idx],pollen=counts[idx])
+
